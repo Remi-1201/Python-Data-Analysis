9dc1461f5b0867602492ecf34e07929460e3945a
Remi-1201 <97021497+Remi-1201@users.noreply.github.com>
2022-05-22 18:30:28 +0900

    add no.11

--- 01.py
+++ 01.py
@@ -175,5 +175,33 @@ def make_cipher_dict(alphabet):
 print("Output for part 3") # Output for part 3 -  note that answers are randomized
 print(make_cipher_dict("")) #{}
 print(make_cipher_dict("cat")) #{'a': 'a', 't': 'c', 'c': 't'}
-print(make_cipher_dict("abcdefghijklmnopqrstuvwxyz "))
-#{'a': 'h', 'l': 'u', 'u': 'q', 'b': 'v', 'y': 'a', 'm': 'r', 'p': 'j', 'k': 'e', 'n': 'p', 't': 'x', 'd': 'o', 'c': 'c', 'w': ' ', 'f': 'd', 'r': 'z', 'v': 'l', 's': 'y', 'e': 'b', 'o': 'i', 'x': 'm', 'h': 's', 'i': 'w', 'q': 'g', 'g': 'n', 'j': 'f', 'z': 'k', ' ': 't'}
+print(make_cipher_dict("abcdefghijklmnopqrstuvwxyz ")) #{'a': 'h', 'l': 'u', 'u': 'q', 'b': 'v', 'y': 'a', 'm': 'r', 'p': 'j', 'k': 'e', 'n': 'p', 't': 'x', 'd': 'o', 'c': 'c', 'w': ' ', 'f': 'd', 'r': 'z', 'v': 'l', 's': 'y', 'e': 'b', 'o': 'i', 'x': 'm', 'h': 's', 'i': 'w', 'q': 'g', 'g': 'n', 'j': 'f', 'z': 'k', ' ': 't'}
+
+
+# 11/ count_letters
+
+def count_letters(word_list):
+    """ See question description """
+    ALPHABET = "abcdefghijklmnopqrstuvwxyz"
+
+    letter_count = {}
+
+    for letter in ALPHABET:
+        letter_count[letter] = 0
+
+    for word in word_list:
+          for letter in word:
+            letter_count[letter]+=1
+    letter_count=sorted(letter_count.items(),reverse=True,key=lambda x:x[1])
+
+    for position in range(0, 26):
+      print (letter_count[position])
+      if position != len(letter_count) - 1:
+       if letter_count[position + 1][1] < letter_count[position][1]:
+                break
+
+monty_quote = "listen strange women lying in ponds distributing swords is no basis for a system of government supreme executive power derives from a mandate from the masses not from some farcical aquatic ceremony"
+
+monty_words = monty_quote.split(" ")
+
+print (count_letters(monty_words))
