d9997eb3ed46953b7ea4824d7a7e4717644b3c4f
Remi-1201 <97021497+Remi-1201@users.noreply.github.com>
2022-05-26 17:33:15 +0900

    Exercises for Nested Data Structures [fixed]

--- 02-5.py
+++ 02-5.py
@@ -150,3 +150,33 @@ print(make_grade_table(name_list, grades_list))
 
 #{'Scott': [75, 59, 89, 77], 'John': [86, 84, 91, 78], 'Joe': [100, 98, 100, 13]}
 
+"""
+11 - Quiz
+"""
+NUM_ROWS = 5
+NUM_COLS = 9
+
+# construct a matrix
+my_matrix = {}
+for row in range(NUM_ROWS):
+    row_dict = {}
+    for col in range(NUM_COLS):
+        row_dict[col] = row * col
+    my_matrix[row] = row_dict
+    
+print(my_matrix)
+#{1: {1: 1, 2: 2, 4: 4, 7: 7, 3: 3, 5: 5, 8: 8, 6: 6, 0: 0}, 4: {5: 20, 7: 28, 2: 8, 6: 24, 1: 4, 3: 12, 8: 32, 0: 0, 4: 16}, 2: {0: 0, 4: 8, 5: 10, 8: 16, 6: 12, 3: 6, 1: 2, 7: 14, 2: 4}, 0: {8: 0, 1: 0, 4: 0, 3: 0, 7: 0, 6: 0, 5: 0, 0: 0, 2: 0}, 3: {2: 6, 6: 18, 4: 12, 0: 0, 7: 21, 5: 15, 1: 3, 8: 24, 3: 9}}
+ 
+# print the matrix
+for row in range(NUM_ROWS):
+    for col in range(NUM_COLS):
+        print(my_matrix[row][col], end = " ")
+    print()
+    
+""" 
+0 0 0 0 0 0 0 0 0 
+0 1 2 3 4 5 6 7 8 
+0 2 4 6 8 10 12 14 16 
+0 3 6 9 12 15 18 21 24 
+0 4 8 12 16 20 24 28 32 
+"""
